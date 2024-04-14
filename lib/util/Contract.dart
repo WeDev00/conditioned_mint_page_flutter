6e6893ed6959477778f485286ab3078b45dc5464
WeDev00 <marcop.dev00@gmail.com>
2024-04-14 18:47:04 +0200

    remove useless file

--- lib/util/Contract.dart
+++
@@ -1,6 +0,0 @@
-
-import 'package:flutter_web3_provider/ethers.dart';
-
-final myInstance = await EthersProvider.onReady;
-final signer = myInstance.getSigner();
-final contract = Contract(deployedContractAddress, signer);
